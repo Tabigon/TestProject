5bef892061ffacbd60703a4d273bb1635ed0862d
Tabigon <Tabigon.artem@gmail.com>
2017-07-13 17:35:39 +0300

    Add new entity Data

---
+++ TestProject.Domain/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProject.Domain
+{
+    public class Data
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string UserData { get; set; }
+    }
+}
